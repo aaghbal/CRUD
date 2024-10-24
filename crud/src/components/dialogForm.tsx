8772daca1c7ac8e2118a35155dc96ee4bf063852
aaghbal <aaghbal@student.1337.ma>
2024-10-24 14:02:04 +0100

    fix: error run

--- crud/src/components/dialogForm.tsx
+++ crud/src/components/dialogForm.tsx
@@ -39,11 +39,11 @@ export function DialogBook({ book }: { book: Book }) {
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Edit Book</DialogTitle>
+          <DialogTitle className="text-white">Edit Book</DialogTitle>
         </DialogHeader>
         <form onSubmit={handleSubmit}>
           <input type="hidden" name="bookId" value={book.id} />
-          <div className="grid gap-4 py-4">
+          <div className="grid gap-4 py-4 text-white">
             <div className="grid grid-cols-4 items-center gap-4">
               <Label htmlFor="title" className="text-right">
                 Title
@@ -52,7 +52,7 @@ export function DialogBook({ book }: { book: Book }) {
                 id="title"
                 name="title"
                 defaultValue={book.title}
-                className="col-span-3"
+                className="col-span-3 text-white"
               />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
@@ -63,7 +63,7 @@ export function DialogBook({ book }: { book: Book }) {
                 id="author"
                 name="author"
                 defaultValue={book.author}
-                className="col-span-3"
+                className="col-span-3 text-white"
               />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
@@ -85,12 +85,12 @@ export function DialogBook({ book }: { book: Book }) {
                 id="language"
                 name="language"
                 defaultValue={book.language}
-                className="col-span-3"
+                className="col-span-3 text-white"
               />
             </div>
           </div>
           <DialogFooter>
-            <Button type="submit">Save changes</Button>
+            <Button type="submit" className="bg-slate-500 text-white">Save changes</Button>
           </DialogFooter>
         </form>
       </DialogContent>
